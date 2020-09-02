1e297e1fb546dff0027d82f406440458df49bd32
Matthew Fuller <matt.j.fuller@outlook.com>
2020-09-02 18:23:30 +0100

    update javascript

--- towel.js
+++ towel.js
@@ -1,7 +1,5 @@
 const price = document.getElementById("price");
 const dimensions = document.getElementById("dimensions");
-const roll1 = document.querySelector(".roll1");
-const roll2 = document.querySelector(".roll2");
 const price2 = document.getElementById("price2");
 const dimensions2 = document.getElementById("dimensions2");
 const tow1 = document.querySelector(".tow1");
@@ -31,18 +29,18 @@ function switchoff2(classname) {
     classname.classList.add("fader");
 }
 
-roll1.addEventListener("mouseover", function() {
-    switchOn1(8.00, 12.5, 9.5, 15, roll1);
+tow1.addEventListener("mouseover", function() {
+    switchOn1(35.00, 37, 13.5, 50, tow1);
 })
 
-roll1.addEventListener("mouseout", function() {
-    switchoff1(roll1);
+tow1.addEventListener("mouseout", function() {
+    switchoff1(tow1);
 })
 
-roll2.addEventListener("mouseover", function() {
-    switchOn2(15.00, 29, 17, 14, roll2);
+tow2.addEventListener("mouseover", function() {
+    switchOn2(8.00, 9.5, 12, 50, tow2);
 })
 
-roll2.addEventListener("mouseout", function() {
-    switchoff2(roll2);
+tow2.addEventListener("mouseout", function() {
+    switchoff2(tow2);
 })
